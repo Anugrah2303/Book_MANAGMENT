354b8c2d674529b9dabc216628f08fc96ed8c7b1
Anugrah <anugrahrakholiya3@gmail.com>
2026-04-02 11:42:03 +0530

    Updated database connection and fixed error

--- config/DBConnection.js
+++ config/DBConnection.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const ConnectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/BookStore')
+    // await mongoose.connect('mongodb://localhost:27017/BookStore')
+    // await mongoose.connect('mongodb+srv://Anugrah:Anugrah1234@cluster0.ziabxxx.mongodb.net/BookStore')
+    await mongoose.connect('mongodb://Anugrah:Anugrah1234@ac-8sqooqg-shard-00-00.ziabxxx.mongodb.net:27017,ac-8sqooqg-shard-00-01.ziabxxx.mongodb.net:27017,ac-8sqooqg-shard-00-02.ziabxxx.mongodb.net:27017/?ssl=true&replicaSet=atlas-fxrg5h-shard-0&authSource=admin&appName=Cluster0/BookStore')
         .then(console.log('DB Connected successfully...'))
         .catch((error) => console.log('Server Error...', error))
 }
